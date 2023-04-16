5fc7e4baa0d36b467546ab028dcceb1e6e2713e4
CoderChirag <jain.chirag0174@gmail.com>
2023-04-16 14:38:56 +0530

    Rendering project Lists

--- Basic Drag and Drop Application/src/app.ts
+++ Basic Drag and Drop Application/src/app.ts
@@ -68,6 +68,44 @@ function AutoBind(
 	return newDescriptor;
 }
 
+// ProjectList Class
+
+class ProjectList {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLElement;
+
+	constructor(private type: 'active' | 'finished') {
+		this.templateElement = document.getElementById(
+			'project-list'
+		)! as HTMLTemplateElement;
+		this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+		const importedNode = document.importNode(
+			this.templateElement.content,
+			true
+		);
+
+		this.element = importedNode.firstElementChild as HTMLElement;
+		this.element.id = `${this.type}-projects`;
+
+		this.attach();
+		this.renderContent();
+	}
+
+	private renderContent() {
+		const listId = `${this.type}-projects-list`;
+		this.element.querySelector('ul')!.id = listId;
+		this.element.querySelector(
+			'h2'
+		)!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement('beforeend', this.element);
+	}
+}
+
 // ProjectInput Class
 class ProjectInput {
 	templateElement: HTMLTemplateElement;
@@ -162,3 +200,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activeProjectsList = new ProjectList('active');
+const finsihedProjectsList = new ProjectList('finished');
